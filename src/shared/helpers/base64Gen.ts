002fc123970fb14a132ac4ed5964bc75f909c2ae
hebertcisco <mrrabbitomg@gmail.com>
2022-04-28 15:50:45 -0300

    v0.0.6

--- src/shared/helpers/base64Gen.ts
+++ src/shared/helpers/base64Gen.ts
@@ -1,5 +1,5 @@
-import * as crypto from "crypto";
-import { VALID_UUID_REGEX } from "../common/constants/regex.constants";
+import * as crypto from 'crypto';
+import { VALID_UUID_REGEX } from '../common/constants/regex.constants';
 
 export type TypeBase64Gen = string | Buffer;
 export type TypeRandomUUID = () => TypeBase64Gen;
@@ -8,12 +8,9 @@ export type TypeValidateUUID = (uuid: string) => boolean;
 export type TypeBase64Encoder = (data: string) => TypeBase64Gen;
 export type TypeBase64Decoder = (data: TypeBase64Gen) => string;
 
-export const base64Gen: TypeBase64Gen = crypto
-  .randomBytes(64)
-  .toString("base64");
+export const base64Gen: TypeBase64Gen = crypto.randomBytes(64).toString('base64');
 
-export const randomUUID: TypeRandomUUID = () =>
-  base64Gen.replace(/\//g, "").replace(/\+/g, "");
+export const randomUUID: TypeRandomUUID = () => base64Gen.replace(/\//g, '').replace(/\+/g, '');
 
 export const validateUUID: TypeValidateUUID = (uuid) => {
   const regex = VALID_UUID_REGEX;
@@ -21,12 +18,12 @@ export const validateUUID: TypeValidateUUID = (uuid) => {
 };
 
 export const base64Decoder: TypeBase64Decoder = (data) => {
-  const buffer = Buffer.from(data.toString(), "base64");
-  return buffer.toString("utf8");
+  const buffer = Buffer.from(data.toString(), 'base64');
+  return buffer.toString('utf8');
 };
 export const base64Encoder: TypeBase64Encoder = (data) => {
   const buffer = Buffer.from(data);
-  return buffer.toString("base64");
+  return buffer.toString('base64');
 };
 
 export default base64Gen;
