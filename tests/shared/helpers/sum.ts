9c9209f729d30ac75e7b36f54ec9e8218ab3225c
hebertcisco <hebertcisco@outlook.com>
2022-05-04 12:35:26 +0000

    v0.2.0

--- tests/shared/helpers/sum.ts
+++
@@ -1,5 +0,0 @@
-export type TypeSum = (arr: number[]) => number;
-
-export const Sum: TypeSum = (arrayOfNumbers) => {
-  return arrayOfNumbers.reduce((firstSide, secondSide) => Number(firstSide) + Number(secondSide), 0);
-};
