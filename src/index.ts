98d340f5e8c9fb0edbeff0c35724fc53ea758da9
hebertcisco <hebertcisco@outlook.com>
2022-06-15 11:59:32 +0000

    refactor: :construction: Munus exportatum est quod numeros temere facit.

--- src/index.ts
+++ src/index.ts
@@ -21,6 +21,7 @@ export const {
   TypeOrmModuleOptionsExtension,
   AppServiceInterface,
   Sum,
+  RandomNumber,
   parseFile,
   date,
   generateAPIKey,
--- src/shared/helpers/index.ts
+++ src/shared/helpers/index.ts
@@ -1,9 +1,11 @@
 export { base64Gen, randomUUID, validateUUID, base64Decoder, base64Encoder } from './crypto/base64Gen';
 export { generateAPIKey } from './crypto/generateAPIKey';
-export { Sum } from './math/sum';
 export { parseFile } from './fs/parseFile';
 import * as date from './time/date-handle';
 export { handleWithAxiosResponse, isSuccessfulRequest } from './http/handleWithAxiosResponse';
 export { parseQueryParams } from './http/parseQueryParams';
+export { RandomNumber } from './math/RandomNumber';
+export { Sum } from './math/sum';
+export type { TypeSum } from './math/sum';
 
 export { date };
