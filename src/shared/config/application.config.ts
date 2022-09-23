ebb40d380eed3dd1fb6de609c868d4230380b8c7
hebertcisco <hebertcisco@outlook.com>
2022-09-23 14:26:53 -0300

    v0.4.5

--- src/shared/config/application.config.ts
+++ src/shared/config/application.config.ts
@@ -3,42 +3,42 @@ import 'dotenv/config';
 
 @Injectable()
 export class ConfigService {
-    public constructor(private env: { [k: string]: string | undefined }) { }
+  public constructor(private env: { [k: string]: string | undefined }) {}
 
-    public getValue(key: string, throwOnMissing = true): string {
-        const value = this.env[key];
-        if (!value && throwOnMissing) {
-            throw new Error(`config error - missing env.${key}`);
-        }
-        return String(value);
+  public getValue(key: string, throwOnMissing = true): string {
+    const value = this.env[key];
+    if (!value && throwOnMissing) {
+      throw new Error(`config error - missing env.${key}`);
     }
+    return String(value);
+  }
 
-    public getBoolean(key: string): boolean {
-        const value = this.getValue(key);
-        return value === 'true';
-    }
+  public getBoolean(key: string): boolean {
+    const value = this.getValue(key);
+    return value === 'true';
+  }
 
-    public ensureValues(keys: string[]): this {
-        keys.forEach((k) => this.getValue(k, true));
-        return this;
-    }
+  public ensureValues(keys: string[]): this {
+    keys.forEach((k) => this.getValue(k, true));
+    return this;
+  }
 
-    public getPort(): number {
-        return Number(this.getValue('PORT', true));
-    }
+  public getPort(): number {
+    return Number(this.getValue('PORT', true));
+  }
 
-    public isProduction(): boolean {
-        const mode = this.getValue('NODE_ENV', true);
-        return mode === 'production';
-    }
-    public isDevelopment(): boolean {
-        const mode = this.getValue('NODE_ENV', true);
-        return mode === 'development';
-    }
-    public isTest(): boolean {
-        const mode = this.getValue('NODE_ENV', true);
-        return mode === 'testing';
-    }
+  public isProduction(): boolean {
+    const mode = this.getValue('NODE_ENV', true);
+    return mode === 'production';
+  }
+  public isDevelopment(): boolean {
+    const mode = this.getValue('NODE_ENV', true);
+    return mode === 'development';
+  }
+  public isTest(): boolean {
+    const mode = this.getValue('NODE_ENV', true);
+    return mode === 'testing';
+  }
 }
 export const configService: ConfigService = new ConfigService(process.env);
 export default ConfigService;
--- src/shared/mocks/javascript/process.mock.ts
+++ src/shared/mocks/javascript/process.mock.ts
@@ -1,10 +1,10 @@
 export class ProcessMock {
-    public env: { [k: string]: string };
-    constructor() {
-        this.env = {
-            PORT: '3333',
-            NODE_ENV: 'testing',
-        };
-    }
+  public env: { [k: string]: string };
+  constructor() {
+    this.env = {
+      PORT: '3333',
+      NODE_ENV: 'testing',
+    };
+  }
 }
 export default new ProcessMock();
