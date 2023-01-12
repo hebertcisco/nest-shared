794a804bec573829d0fcf937f55bd46d69a8b4c5
hebertcisco <hebertcisco@outlook.com>
2023-01-12 00:50:36 +0000

    v0.4.60

--- src/shared/helpers/crypto/Base64.ts
+++ src/shared/helpers/crypto/Base64.ts
@@ -1,44 +1,30 @@
 import crypto from 'node:crypto';
-import { BufferBase } from '../../contract/base';
+import { Base64 as B64 } from '@localtools/base64';
 
 import { VALID_UUID_REGEX } from '../../constants';
 
-import type { DecodeEncodingType, DecodeStrType, EncodeDataType } from '../../contract/types/buffer.type';
+import type { DecodeEncodingType, DecodeStrType } from '../../contract/types/buffer.type';
 
-export class Base64 implements BufferBase {
-  public encode(data: EncodeDataType) {
-    const buffer = Buffer.from(data);
-    return buffer.toString('base64');
-  }
-  public decode(str: DecodeStrType): string {
-    const buffer = Buffer.from(str.toString(), 'base64');
-    return buffer.toString('utf8');
-  }
+export class Base64 {
   public decodeToBuffer(str: DecodeStrType, encoding: DecodeEncodingType = 'utf8') {
     return Buffer.from(str.toString(), encoding);
   }
   public validateUUID(uuid: string) {
     return VALID_UUID_REGEX.test(uuid);
   }
-  public randomUUID() {
-    const uuid = crypto.randomUUID();
-    if (!VALID_UUID_REGEX.test(uuid)) {
-      throw new Error(`Invalid UUID: ${uuid}`);
-    }
-    return uuid;
-  }
 }
 
 export const base64 = new Base64();
-export const base64Gen: string = base64.encode(base64.randomUUID());
 
-export const randomUUID: string = base64.randomUUID();
-
+export const randomUUID: string = crypto.randomUUID();
+export const base64Gen: string = B64.encode({
+  text: randomUUID,
+});
 export const decodeToBuffer = base64.decodeToBuffer;
 
 export const validateUUID = base64.validateUUID;
 
-export const encode = base64.encode;
-export const decode = base64.decode;
+export const encode = B64.encode;
+export const decode = B64.decode;
 
 export default base64;
