b3951852ca685070e35d67a724bb1cdd632c338a
hebertcisco <hebertcisco@outlook.com>
2023-01-20 18:32:48 +0000

    v5.0.0

--- src/shared/helpers/time/__tests__/date-handle.ts
+++ src/shared/helpers/time/__tests__/date-handle.ts
@@ -1,6 +1,4 @@
-import { future, getAgeFromDate, getRandomBirthDate, past, yesterday } from '../../..';
-import { DateHandleImpl } from '../../..';
-import { date } from '../../..';
+import { dateHandle } from '../../..';
 
 describe('DateHandle', () => {
   let staticDate: Date;
@@ -11,49 +9,21 @@ describe('DateHandle', () => {
     jest.spyOn(Date, 'now').mockImplementation(() => staticDate.getTime());
     jest.useFakeTimers().setSystemTime(staticDate);
   });
-  test('should return an object with the correct type and name', () => {
-    const result = getAgeFromDate(staticDate);
-    expect(result).toBe(-53);
-  });
 
   test('dayOfWeek', () => {
-    const dateHandle = new DateHandleImpl();
     const result = dateHandle.dayOfWeek(Date.now());
     expect(result).toBeFalsy();
   });
   test('isSaturday', () => {
-    const dateHandle = new DateHandleImpl();
     const result = dateHandle.isSaturday();
     expect(result).toBeFalsy();
   });
   test('isSunday', () => {
-    const dateHandle = new DateHandleImpl();
     const result = dateHandle.isSunday();
     expect(result).toBeFalsy();
   });
   test('isMonday', () => {
-    const dateHandle = new DateHandleImpl();
     const result = dateHandle.isMonday();
     expect(result).toBeFalsy();
-  });
-  test('getRandomBirthDate', () => {
-    const result = getRandomBirthDate(staticDate);
-    expect(result).toBeInstanceOf(Date);
-  });
-  test('future', () => {
-    const result = future(1, 'days');
-    expect(result).toBeDefined();
-  });
-  test('past', () => {
-    const result = past(1, 'days');
-    expect(result).toBeDefined();
-  });
-
-  test('date', () => {
-    expect(date).toBeInstanceOf(Object);
-  });
-  test('yesterday', () => {
-    const result = yesterday();
-    expect(result).toBeDefined();
   });
 });
