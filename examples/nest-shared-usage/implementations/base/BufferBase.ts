8394d9c2c9607990b4529366ccbddfee437d2c7a
hebertcisco <mrrabbitomg@gmail.com>
2023-04-20 22:09:48 -0300

    update BufferBase.ts

--- examples/nest-shared-usage/implementations/base/BufferBase.ts
+++ examples/nest-shared-usage/implementations/base/BufferBase.ts
@@ -1,24 +1,24 @@
-import { BufferBase } from 'nest-shared'
-import { Buffer } from 'buffer'
-import type { EncodeDataType, DecodeStrType } from 'nest-shared/lib/shared/types/buffer.type'
+import { BufferBase } from 'nest-shared';
+import { Buffer } from 'buffer';
+import { EncodeDataType, DecodeStrType } from 'nest-shared/lib/shared/contract/types/buffer.type';
 
-console.log(BufferBase.name) // BufferBase
+console.log(BufferBase.name); // BufferBase
 
 class BufferBaseImpl implements BufferBase {
-    encode(data: EncodeDataType): string {
-        return Buffer.from(data).toString('base64')
-    }
-    decode(str: DecodeStrType): string {
-        return Buffer.from(str, 'utf-8').toString("utf-8")
-    }
+  encode(data: EncodeDataType): string {
+    return Buffer.from(data).toString('base64');
+  }
+  decode(str: DecodeStrType): string {
+    return Buffer.from(str, 'utf-8').toString('utf-8');
+  }
 }
 
-const bufferBaseImpl = new BufferBaseImpl()
+const bufferBaseImpl = new BufferBaseImpl();
 
-const content = "Hello World!"
+const content = 'Hello World!';
 
-const encoded = bufferBaseImpl.encode(content)
-const decoded = bufferBaseImpl.decode(content)
+const encoded = bufferBaseImpl.encode(content);
+const decoded = bufferBaseImpl.decode(content);
 
-console.log('encoded', encoded) // SGVsbG8gV29ybGQh
-console.log('decoded', decoded) // Hello World!
+console.log('encoded', encoded); // SGVsbG8gV29ybGQh
+console.log('decoded', decoded); // Hello World!
