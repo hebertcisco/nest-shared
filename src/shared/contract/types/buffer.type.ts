37d4e0f106775227f4eaacc36e24019dcdee6a55
hebertcisco <hebertcisco@outlook.com>
2025-09-13 18:12:57 -0300

    v5.0.9

--- src/shared/contract/types/buffer.type.ts
+++ src/shared/contract/types/buffer.type.ts
@@ -1,9 +1,6 @@
 // Local replacement for the global `WithImplicitCoercion<T>` helper type
 // to support environments where it is not available in lib typings.
-type ImplicitCoercion<T> =
-  | T
-  | { valueOf(): T }
-  | { [Symbol.toPrimitive](hint: 'string'): T };
+type ImplicitCoercion<T> = T | { valueOf(): T } | { [Symbol.toPrimitive](hint: 'string'): T };
 
 export type EncodeDataType = ImplicitCoercion<string | Uint8Array | readonly number[]>;
 
